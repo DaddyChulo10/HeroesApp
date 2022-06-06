b4fa340038ceca0230bc7bc659925e282f39c6ec
DaddyChulo10 <edgtarkalim@gmail.com>
2022-06-06 18:35:31 -0500

    Protección de rutas CanLoad/CanActivate

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styles: [
-  ]
+  styles: [ `` ]
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  constructor(private router: Router, private authService: AuthService) { }
 
-  ngOnInit(): void {
+  login() {
+    this.authService.login().subscribe(auth => { if (auth.id) { this.router.navigate(['./heroes']) } })
+    //this.router.navigate(['/heroes']);
   }
 
 }
