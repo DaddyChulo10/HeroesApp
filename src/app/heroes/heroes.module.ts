9f7e292f2a2d4f05ae734a3057073906cd62318c
DaddyChulo10 <edgtarkalim@gmail.com>
2022-06-05 19:38:13 -0500

    Buscador y card del buscador

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -10,6 +10,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from '../material/material.module';
 import { HeroeTarjetaComponentsComponent } from './components/heroe-tarjeta-components/heroe-tarjeta-components.component';
 import { ImagenPipe } from './pipes/imagen.pipe';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -28,7 +29,8 @@ import { ImagenPipe } from './pipes/imagen.pipe';
     CommonModule,
     AppRoutingModule,
     FlexLayoutModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule,
   ]
 })
 export class HeroesModule { }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,19 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Heroe } from '../interfaces/heroes.interfaces';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesService {
 
+  private baseUrl: string = environment.baseUrl;
+
   constructor(private http: HttpClient) { }
   getHeroes() {
-    return this.http.get<Heroe[]>('http://localhost:3000/heroes')
+    return this.http.get<Heroe[]>(this.baseUrl + '/heroes')
   }
 
   getHeroePorId(id: string) {
-    return this.http.get<Heroe>(`http://localhost:3000/heroes/${id}`)
-  } 
+    return this.http.get<Heroe>(this.baseUrl + `/heroes/${id}`)
+  }
 
+  getSugerencias(termino: string): Observable<Heroe[]> {
+    return this.http.get<Heroe[]>(this.baseUrl + `/heroes/?q=${termino}&_limit=6`)
+  }
 }
